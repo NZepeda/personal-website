105e39160b5be829930ecd0a774465ce6568b1ca
Nestor Zepeda <NestorDZepeda@gmail.com>
2017-06-30 16:33:11 -0700

    Removed 500

--- server/controllers/systemController.js
+++ server/controllers/systemController.js
@@ -5,7 +5,7 @@ exports.postUrlToDb = function(req, res){
     var newData = new PageData({url: req.query.pageUrl, word: req.query.word});
     newData.save(function(err, data){
         if(err){
-            res.status(500).send('Error posting to the database!');
+            res.status(200).send('');
         }
         else{
             res.setHeader('Content-Type', 'application/json');
